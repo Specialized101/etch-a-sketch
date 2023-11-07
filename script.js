2d3c2a5fc14614b4c9a305a5a9b83b5e684c9401
Specialized101 <yassinefares17@gmail.com>
2023-11-07 14:23:37 +0100

    Add button to adjust the width of the grid

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 const container = document.querySelector('#container');
-const squaresPerLine = 16;
+let squaresPerLine = 16;
 const containerWidth = 960;
+const promptBtn = document.createElement('button');
 
-container.style.width = `${containerWidth}px`;
-container.style.height = container.style.width;
+promptBtn.textContent = 'Ajust the number of squares per side';
+promptBtn.setAttribute('id', 'promptBtn');
+promptBtn.addEventListener('click', () => {
 
-for(let i = 0; i < squaresPerLine * squaresPerLine; i++){
-    let div = document.createElement('div');
+    let input = prompt("Enter a number between 1 and 100");
+    if (input === null || input === NaN) return;
 
-    div.classList.add('square');
-    div.style.width = `${containerWidth / squaresPerLine}px`;
-    div.style.height = div.style.width;
-    
-    div.addEventListener('pointerover', () => {
-        div.style.backgroundColor = 'lightblue';
+    while (parseInt(input) <= 0 || parseInt(input) > 100) {
+        input = prompt("Please make sure your number is between 1 and 100");
+    }
+
+    squaresPerLine = parseInt(input);
+
+    let containerChilds = document.querySelectorAll('.square');
+    containerChilds.forEach(child => {
+        container.removeChild(child);
     });
 
-    container.appendChild(div);
-}
+    container.style.width = `${containerWidth}px`;
+    container.style.height = container.style.width;
+
+    for (let i = 0; i < squaresPerLine * squaresPerLine; i++) {
+        let div = document.createElement('div');
+
+        div.classList.add('square');
+        div.style.width = `${containerWidth / squaresPerLine}px`;
+        div.style.height = div.style.width;
+
+        div.addEventListener('pointerover', () => {
+            div.style.backgroundColor = 'lightblue';
+        });
+
+        container.appendChild(div);
+    }
+});
+
+container.appendChild(promptBtn);
+
+
+
